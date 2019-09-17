5e96a1d878d3a5c1c3a9326ed86b2ddcf48b8616
MRtecno98 <mr.tecno98@gmail.com>
2019-09-17 21:24:26 +0200

    Updated __init__

--- src/algebra/__init__.py
+++ src/algebra/__init__.py
@@ -20,4 +20,4 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 """
 
-from .algebra import Number, Variable, Monomial, Polinomial
+from .algebra import *
